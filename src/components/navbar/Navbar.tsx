d2ecce6fae1b1d1b739f68a6179121c525fe4db4
dozin <dojin0345@gmail.com>
2025-12-12 19:13:07 +0900

    refactor : authlink와 navbar를 통합한 후 화면 작아졌을 때 제대로 버거 메뉴가 만들어지지 않는 문제 수정

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,29 +2,93 @@
 import React from "react";
 import styles from "./navbar.module.css";
 import Link from "next/link";
-import Image from "next/image";
-import AuthLinks from "../authLinks/AuthLinks";
 import ThemeToggle from "../themeToggle/ThemeToggle";
 import { useSession } from "next-auth/react";
 
 const Navbar = () => {
-  const session = useSession();
+  const [open, setOpen] = React.useState(false);
+  const { status } = useSession();
+
   return (
     <div className={styles.container}>
       <Link href="/" className={styles.logo}>
         zindozang
       </Link>
       <div className={styles.links}>
-        <Link href="/tags" className={styles.tags}>
-          TAGS
+        <Link href="/tags" className={styles.link}>
+          Tags
         </Link>
-        <Link href="/" className={styles.about}>
-          ABOUT
+        <Link href="/" className={styles.link}>
+          About
         </Link>
-        {session.status === "authenticated" && <AuthLinks />}
+        {status === "unauthenticated" ? (
+          <Link href="/login" className={styles.link}>
+            Login
+          </Link>
+        ) : (
+          <>
+            <Link href="/write" className={styles.link}>
+              Write
+            </Link>
+            <Link href="/logout" className={styles.link}>
+              Logout
+            </Link>
+          </>
+        )}
+
+        <div className={styles.burger} onClick={() => setOpen(!open)}>
+          <div className={styles.line}></div>
+          <div className={styles.line}></div>
+          <div className={styles.line}></div>
+        </div>
         <ThemeToggle />
+        {open && (
+          <div className={styles.responsiveMenu}>
+            <Link
+              href="/tags"
+              className={styles.burgerLink}
+              onClick={() => setOpen(false)}
+            >
+              Tags
+            </Link>
+            <Link
+              href="/"
+              className={styles.burgerLink}
+              onClick={() => setOpen(false)}
+            >
+              About
+            </Link>
+            {status === "unauthenticated" ? (
+              <Link
+                href="/login"
+                className={styles.burgerLink}
+                onClick={() => setOpen(false)}
+              >
+                Login
+              </Link>
+            ) : (
+              <>
+                <Link
+                  href="/write"
+                  className={styles.burgerLink}
+                  onClick={() => setOpen(false)}
+                >
+                  Write
+                </Link>
+                <Link
+                  href="/logout"
+                  className={styles.burgerLink}
+                  onClick={() => setOpen(false)}
+                >
+                  Logout
+                </Link>
+              </>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
 };
+
 export default Navbar;
